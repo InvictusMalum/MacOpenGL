9e4a0e3efe7ebeac4d2ea7f75eb5ad8f7ebe34ff
Baret Buckley <58374398+thejocky@users.noreply.github.com>
2022-10-24 09:04:34 -0400

    Started and mostly finished adding window registry for callbacks

--- include/Window.hpp
+++ include/Window.hpp
@@ -6,7 +6,32 @@ namespace gl{
 class Window;
 }
 
+#include <map>
+
 namespace gl {
+
+    class WindowRegistry {
+        using Window::framebuffer_size_callback framebufferCallback;
+        
+        std::map<GLFWwidow*, Window*>
+            windowMap_;
+        
+        public:
+        
+        WindowRegistry() {}
+        ~WindowRegistry() {}
+
+        protected:
+
+        bool registerWindow(GLFWwindow* glfwWindow, Window* window);
+        void unregisterWindow(GLFWwindow* glfwWindow);
+        Window* getWindow(GLFWwindow* window) {return windowMap_[window];}
+
+        private:
+
+        static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+    };
     
 
     class Window {
@@ -24,11 +49,13 @@ namespace gl {
         int winWidth_, winHeight_;
         float ratio_;
 
-
         public:
         Window(WindowType type, int windowWidth = 0,
                     int windowHeight = 0, const char* title = "",
                     void (*key_callback)(GLFWwindow*,int,int,int,int) = nullptr);
+
+        // Callbacks
+        void framebuffer_size_callback(int width, int height)
         
         void setDimensions(int width, int height);
         void processInput();
--- source/Window.cpp
+++ source/Window.cpp
@@ -5,57 +5,68 @@
 
 namespace gl {
 
-        // enum WindowType {
-        //     static_window,
-        //     stretch_window,
-        //     scale_window,
-        //     dynamic_window
-        // };
+    WindowRegistry g_windowRegistry();
 
-        // GLFWwindow* window_;
-        // WindowType type_;
-        // int width, height;
-        // int winWidth_, winHeight_;
-        // float ratio_
-
-
-        // Callback for adjusting view window size
-        void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-            glViewport(0, 0, width, height);
-        }
-
-        Window::Window(WindowType type, int windowWidth,
-                int windowHeight, const char* title,
-                void (*key_callback) (GLFWwindow*,int,int,int,int)) :
-            type_(type), winWidth_(windowWidth), winHeight_(windowHeight)
-        {
-            window_ = glfwCreateWindow(winWidth_, winHeight_, title, nullptr, nullptr);
-            if (window_ == nullptr)
-                throw std::runtime_error("Failed To Create GLFW Window");
-
-            glfwMakeContextCurrent(window_);
-            glfwSetFramebufferSizeCallback(window_, framebuffer_size_callback);
-
-            if (key_callback)
-                glfwSetKeyCallback(window_, key_callback);
-            
-        }
-
+    bool WindowRegistry::registerWindow(GLFWwindow* glfwWindow,
+                                        Window* window) {
+        if (glfwWindow && window &&
+                windowMap_[glfwWindow])
+            return false;
         
-        void Window::setDimensions(int width, int height) {
-            // Manually run callback function to adjust sizing of the view port
-            // UNTESTED IF WORKS PROPERLY
-            framebuffer_size_callback(window_, width, height);
-        }
-        void Window::swapBuffers() {
-            glfwSwapBuffers(window_);
-        }
-        bool Window::shouldClose() {
-            return glfwWindowShouldClose(window_);
-        }
-        void Window::setWindowShouldClose(bool state) {
-            glfwSetWindowShouldClose(window_, state);
-        }
+        windowMap_[glfwWindow] = window;
+        return true;
+    }
+
+    void WindowRegistry::unregisterWindow(GLFWwindow* glfwWindow) {
+        windowMap_[glfwWindow] = nullptr;
+    }
+
+    static void framebuffer_size_callback
+            (GLFWwindow* window, int width, int height) {
+        windowMap_[window].framebufferCallback(width, height)
+    }
+
+
+    Window::Window(WindowType type, int windowWidth,
+            int windowHeight, const char* title,
+            void (*key_callback) (GLFWwindow*,int,int,int,int)) :
+        type_(type), winWidth_(windowWidth), winHeight_(windowHeight)
+    {
+        window_ = glfwCreateWindow(winWidth_, winHeight_, title, nullptr, nullptr);
+        if (window_ == nullptr)
+            throw std::runtime_error("Failed To Create GLFW Window");
+
+        // Register window with global registry so this class 
+        // recieves callbacks from them.
+        g_windowRegistry.registerWindow(window_, this);
+
+        glfwMakeContextCurrent(window_);
+        glfwSetFramebufferSizeCallback(window_, framebuffer_size_callback);
+
+        if (key_callback)
+            glfwSetKeyCallback(window_, key_callback);
+        
+    }
+
+    // Callback for adjusting view window size
+    void Window::framebuffer_size_callback(int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+    
+    void Window::setDimensions(int width, int height) {
+        // Manually run callback function to adjust sizing of the view port
+        // UNTESTED IF WORKS PROPERLY
+        framebuffer_size_callback(window_, width, height);
+    }
+    void Window::swapBuffers() {
+        glfwSwapBuffers(window_);
+    }
+    bool Window::shouldClose() {
+        return glfwWindowShouldClose(window_);
+    }
+    void Window::setWindowShouldClose(bool state) {
+        glfwSetWindowShouldClose(window_, state);
+    }
 
 
 }
